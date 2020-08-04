a487949ff23dfb8407b5786bd887a5a8aaf6698d
Nagihan Ustun <nagihan.ustun99@gmail.com>
2020-08-04 22:34:49 +0300

    .

--- youtubeApi/youtubeApi/Youtube.Designer.cs
+++ youtubeApi/youtubeApi/Youtube.Designer.cs
@@ -30,12 +30,12 @@
         {
             this.SuspendLayout();
             // 
-            // Form1
+            // Youtube
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
+            this.Name = "Youtube";
             this.Text = "Youtube";
             this.ResumeLayout(false);
 
