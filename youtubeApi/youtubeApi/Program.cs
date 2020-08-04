eace59d33aa886c52955d7fdd4018cca135bc3ec
Nagihan Ustun <nagihan.ustun99@gmail.com>
2020-08-04 22:15:29 +0300

    Form1 is renamed as Youtube.

--- youtubeApi/youtubeApi/Program.cs
+++ youtubeApi/youtubeApi/Program.cs
@@ -16,7 +16,7 @@ namespace youtubeApi
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Youtube());
         }
     }
 }
--- youtubeApi/youtubeApi/Youtube.cs
+++ youtubeApi/youtubeApi/Youtube.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace youtubeApi
 {
-    public partial class Form1 : Form
+    public partial class Youtube : Form
     {
-        public Form1()
+        public Youtube()
         {
             InitializeComponent();
         }
